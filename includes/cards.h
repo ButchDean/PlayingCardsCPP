3fc770b4c83b057a5bae32a34867c9eca3f402eb
Dean Butcher <butchdeanca@gmail.com>
2015-06-28 17:45:45 -0300

    Fixed conflicts regarding adding unordered_map()

--- includes/cards.h
+++ includes/cards.h
@@ -2,7 +2,7 @@
 #define _CARDS_MECHANIC_
 
 #include <deque>
-#include <map>
+#include <unordered_map>
 #include <string>
 
 namespace cards
@@ -11,7 +11,13 @@ namespace cards
 			CLUBS_A, CLUBS2, CLUBS3, CLUBS4, CLUBS5, CLUBS6, CLUBS7, CLUBS8, CLUBS9, CLUBS10, CLUBS_J, CLUBS_Q, CLUBS_K,
 			HEARTS_A, HEARTS2, HEARTS3, HEARTS4, HEARTS5, HEARTS6, HEARTS7, HEARTS8, HEARTS9, HEARTS10, HEARTS_J, HEARTS_Q, HEARTS_K,
 			DIAMONDS_A, DIAMONDS2, DIAMONDS3, DIAMONDS4, DIAMONDS5, DIAMONDS6, DIAMONDS7, DIAMONDS8, DIAMONDS9, DIAMONDS10, DIAMONDS_J, DIAMONDS_Q, DIAMONDS_K,
-			EMPTY_DECK };	
+			EMPTY_DECK };
+			
+	struct CardDetail
+	{
+	    std::string cardStr;
+	    int cardVal;
+	};	
 	
 	class CCardDeck
 	{
@@ -28,8 +34,7 @@ namespace cards
 			
 		private:
 			std::deque<CardRefs> deck;
-			std::map<CardRefs, std::string> cardString;
-			std::map<CardRefs, int> cardValue;
+			std::unordered_map<int, CardDetail> cardDetail;
 			
 			int _GetRandomCardPos();	
 	};
--- src/cards.cpp
+++ src/cards.cpp
@@ -22,59 +22,64 @@ namespace cards
 		
 		std::printf("Deck size: %lu\n", deck.size());
 		
-		// Set up strings for card enums.
-		cardString[SPADES_A] = "SPADES_A";
-		cardString[SPADES2] = "SPADES2";
-		cardString[SPADES3] = "SPADES3";
-		cardString[SPADES4] = "SPADES4";
-		cardString[SPADES5] = "SPADES5";
-		cardString[SPADES6] = "SPADES6";
-		cardString[SPADES7] = "SPADES7";
-		cardString[SPADES8] = "SPADES8";
-		cardString[SPADES9] = "SPADES9";
-		cardString[SPADES10] = "SPADES10";
-		cardString[SPADES_J] = "SPADES_J";
-		cardString[SPADES_Q] = "SPADES_Q";
-		cardString[SPADES_K] = "SPADES_K";
-		cardString[CLUBS_A] = "CLUBS_A";
-		cardString[CLUBS2] = "CLUBS2";
-		cardString[CLUBS3] = "CLUBS3";
-		cardString[CLUBS4] = "CLUBS4";
-		cardString[CLUBS5] = "CLUBS5";
-		cardString[CLUBS6] = "CLUBS6";
-		cardString[CLUBS7] = "CLUBS7";
-		cardString[CLUBS8] = "CLUBS8";
-		cardString[CLUBS9] = "CLUBS9";
-		cardString[CLUBS10] = "CLUBS10";
-		cardString[CLUBS_J] = "CLUBS_J";
-		cardString[CLUBS_Q] = "CLUBS_Q";
-		cardString[CLUBS_K] = "CLUBS_K";
-		cardString[HEARTS_A] = "HEARTS_A";
-		cardString[HEARTS2] = "HEARTS2";
-		cardString[HEARTS3] = "HEARTS3";
-		cardString[HEARTS4] = "HEARTS4";
-		cardString[HEARTS5] = "HEARTS5";
-		cardString[HEARTS6] = "HEARTS6";
-		cardString[HEARTS7] = "HEARTS7";
-		cardString[HEARTS8] = "HEARTS8";
-		cardString[HEARTS9] = "HEARTS9";
-		cardString[HEARTS10] = "HEARTS10";
-		cardString[HEARTS_J] = "HEARTS_J";
-		cardString[HEARTS_Q] = "HEARTS_Q";
-		cardString[HEARTS_K] = "HEARTS_K";
-		cardString[DIAMONDS_A] = "DIAMONDS_A";
-		cardString[DIAMONDS2] = "DIAMONDS2";
-		cardString[DIAMONDS3] = "DIAMONDS3";
-		cardString[DIAMONDS4] = "DIAMONDS4";
-		cardString[DIAMONDS5] = "DIAMONDS5";
-		cardString[DIAMONDS6] = "DIAMONDS6";
-		cardString[DIAMONDS7] = "DIAMONDS7";
-		cardString[DIAMONDS8] = "DIAMONDS8";
-		cardString[DIAMONDS9] = "DIAMONDS9";
-		cardString[DIAMONDS10] = "DIAMONDS10";
-		cardString[DIAMONDS_J] = "DIAMONDS_J";
-		cardString[DIAMONDS_Q] = "DIAMONDS_Q";
-		cardString[DIAMONDS_K] = "DIAMONDS_K";		
+		// Set up strings and values for card enums.
+		cardDetail.clear();
+		
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES_A, {"SPADES_A", 21}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES2, {"SPADES2", 2}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES3, {"SPADES3", 3}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES4, {"SPADES4", 4}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES5, {"SPADES5", 5}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES6, {"SPADES6", 6}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES7, {"SPADES7", 7}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES8, {"SPADES8", 8}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES9, {"SPADES9", 9}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES10, {"SPADES10", 10}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES_J, {"SPADES_J", 11}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES_Q, {"SPADES_Q", 12}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(SPADES_K, {"SPADES_K", 13}));
+		
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS_A, {"CLUBS_A", 21}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS2, {"CLUBS2", 2}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS3, {"CLUBS3", 3}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS4, {"CLUBS4", 4}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS5, {"CLUBS5", 5}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS6, {"CLUBS6", 6}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS7, {"CLUBS7", 7}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS8, {"CLUBS8", 8}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS9, {"CLUBS9", 9}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS10, {"CLUBS10", 10}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS_J, {"CLUBS_J", 11}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS_Q, {"CLUBS_Q", 12}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(CLUBS_K, {"CLUBS_K", 13}));
+		
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS_A, {"HEARTS_A", 21}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS2, {"HEARTS2", 2}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS3, {"HEARTS3", 3}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS4, {"HEARTS4", 4}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS5, {"HEARTS5", 5}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS6, {"HEARTS6", 6}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS7, {"HEARTS7", 7}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS8, {"HEARTS8", 8}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS9, {"HEARTS9", 9}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS10, {"HEARTS10", 10}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS_J, {"HEARTS_J", 11}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS_Q, {"HEARTS_Q", 12}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(HEARTS_K, {"HEARTS_K", 13}));
+		
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS_A, {"DIAMONDS_A", 21}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS2, {"DIAMONDS2", 2}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS3, {"DIAMONDS3", 3}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS4, {"DIAMONDS4", 4}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS5, {"DIAMONDS5", 5}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS6, {"DIAMONDS6", 6}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS7, {"DIAMONDS7", 7}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS8, {"DIAMONDS8", 8}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS9, {"DIAMONDS9", 9}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS10, {"DIAMONDS10", 10}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS_J, {"DIAMONDS_J", 11}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS_Q, {"DIAMONDS_Q", 12}));
+		cardDetail.insert(std::make_pair<int, CardDetail>(DIAMONDS_K, {"DIAMONDS_K", 13}));
 	}
 	
 	int CCardDeck::_GetRandomCardPos()
@@ -111,12 +116,7 @@ namespace cards
 			deck[posA] = cardValB;
 			deck[posB] = cardValA;
 		}
-		
-		for(int i = SPADES_A; i != EMPTY_DECK; i++)
-		{
-			std::printf("%s\n", CardToStr(deck[i]).c_str());
-		}
-		
+
 		std::printf("Shuffled deck size: %lu\n", deck.size());		
 	}
 	
@@ -139,90 +139,24 @@ namespace cards
 	
 	std::string CCardDeck::CardToStr(const CardRefs card) const
 	{
-		auto strCard = cardString.find(card);
+		auto cardSearch = cardDetail.find(card);
 		
-		if(strCard == cardString.end())
-			return "<INVALID CARD IN DECK>";
-		else
-			return strCard->second;
+		if(cardSearch == cardDetail.end())
+			return "Card string not found!";
+			
+		return cardSearch->second.cardStr;
 	}
 	
 	int CCardDeck::CardValue(const CardRefs card) const
 	{
-		// Example scoring. Could also be changed to read from an XML file too maybe.
-		// That is on my todo list.
-		switch(card)
-		{
-			case SPADES_A:
-			case CLUBS_A:
-			case HEARTS_A:
-			case DIAMONDS_A:
-				return 21;
-			case SPADES2:
-			case CLUBS2:
-			case HEARTS2:
-			case DIAMONDS2:
-				return 2;
-			case SPADES3:
-			case CLUBS3:
-			case HEARTS3:
-			case DIAMONDS3:
-				return 3;
-			case SPADES4:
-			case CLUBS4:
-			case HEARTS4:
-			case DIAMONDS4:
-				return 4;
-			case SPADES5:
-			case CLUBS5:
-			case HEARTS5:
-			case DIAMONDS5:
-				return 5;
-			case SPADES6:
-			case CLUBS6:
-			case HEARTS6:
-			case DIAMONDS6:
-				return 6;
-			case SPADES7:
-			case CLUBS7:
-			case HEARTS7:
-			case DIAMONDS7:
-				return 7;
-			case SPADES8:
-			case CLUBS8:
-			case HEARTS8:
-			case DIAMONDS8:
-				return 8;
-			case SPADES9:
-			case CLUBS9:
-			case HEARTS9:
-			case DIAMONDS9:
-				return 9;
-			case SPADES10:
-			case CLUBS10:
-			case HEARTS10:
-			case DIAMONDS10:
-				return 10;
-			case SPADES_J:
-			case CLUBS_J:
-			case HEARTS_J:
-			case DIAMONDS_J:
-				return 11;
-			case SPADES_Q:
-			case CLUBS_Q:
-			case HEARTS_Q:
-			case DIAMONDS_Q:
-				return 12;
-			case SPADES_K:
-			case CLUBS_K:
-			case HEARTS_K:
-			case DIAMONDS_K:
-				return 13;
-			case EMPTY_DECK:
-				return 0;
-			default:
-				return 0;
-		}		
+		auto cardSearch = cardDetail.find(card);
 		
+		if(cardSearch == cardDetail.end())
+		{
+			std::puts("Card value not found!");
+			return -1;
+		}
+			
+		return cardSearch->second.cardVal;
 	}
 }
